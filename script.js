b616fd7f5b88697b47d9e99de72d18a552ca08c0
syahrulangwar <syahrulangwar1@gmail.com>
2026-06-14 10:53:30 +0700

    Menerapkan array pada js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*  hanya berfungsi untuk 1 data dan tidak effisien jika banyak data
+
 let tombolBeliElement = document.getElementById("tombol-beli");
 let stokDisplayElement = document.getElementById("stok-display");
 
@@ -5,8 +7,8 @@ const STOK_AWAL = 10;
 let jumlahKlik = 0;
 
 tombolBeliElement.addEventListener("click", function(){
-    if(stokSisa >= STOK_AWAL){
-        alert("stok habis");
+    if(jumlahKlik >= STOK_AWAL){
+        alert("sorry stoknya habis kak");
         return;
     }
 
@@ -14,6 +16,60 @@ tombolBeliElement.addEventListener("click", function(){
     let stokSisa = STOK_AWAL - jumlahKlik;
 
     stokDisplayElement.innerText = "stok sisa : " + stokSisa;
-    tombolBeliElement.innerText = "Dibeli (" + jumlahKlik + ")x";
-    alert("produkberhasil dibeli");
-}); 
+    alert("sudah masuk keranjang");
+}); */
+
+// sebuah simulasi database menggunakan array
+// lebih effisien untuk banyak data sekaligus
+const DAFTAR_PRODUK = [
+    {
+        id: 1,
+        nama: "laptop gaming",
+        harga: 10000,
+        stok: 2
+    },
+
+    {
+        id: 2,
+        nama: "laptop sultan",
+        harga: 50000,
+        stok: 5
+    },
+
+    {
+        id: 3,
+        nama: "laptop murah",
+        harga: 1000,
+        stok: 1
+    },
+
+    {
+        id: 4,
+        nama: "laptop keren",
+        harga: 10000,
+        stok: 5
+    },
+];
+
+// membuat fungsi untuk mencari produk berdasarkan id
+function beliProduk(idProduk){
+    const produk = DAFTAR_PRODUK.find(p => p.id === idProduk);
+
+    if(!produk){
+        alert("Maaf, produk tidak ditemukan");
+        return;
+    }
+
+    if(produk.stok <= 0){
+        alert("Produk " + produk.nama + " sudah habis");
+        return;
+    }
+
+    // otomatis mengurangi stok array
+    produk.stok--;
+    alert("Berhasil masuk ke keranjang");
+
+    //mengambil data berdasarkan id dan mengubah nya sesuai dengan stok di array
+    const stokDisplayElement = document.getElementById("stok-" + idProduk);
+    stokDisplayElement.innerText = "stok sisa : " + produk.stok;
+}
